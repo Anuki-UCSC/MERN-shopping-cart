1ff10648e6689e33ac4f13bc1483e6784b74f8ac
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-16 10:26:14 +0530

    database created, inserted data

---
+++ backend/data/products.js
@@ -0,0 +1,37 @@
+const products=[
+    {
+        name:'JAVA RED VELVET CAKE WITH CREAM CHEESE HEART',
+        description:"This classic Moist red velvet sandwiched and also covered with cream cheese frosting topped with red velvet cake crumbs will be a sure treat to surprise your sweetheart for valentine`s day, anniversary or birthday. Java lounge is an upscale coffee lounge chain in Sri Lanka with high quality coffee based beverages, cakes, desserts and snacks. It`s desserts and cakes are rated 5 star with a strong backing of the Java Lounge brand",
+        price:3300,
+        imageUrl:"https://www.kapruka.com/cdn-cgi/image/width=340,quality=92,f=auto/shops/cakes/productImages/zoom/1610716378648_DSC_6301_M.jpg",
+    },
+    {
+        name:'FLOWER BOUQUET',
+        description:"All our arrangements are made to order with fresh flowers. Occasionally we may face difficulties in arranging the same flowers indicated in the image due to shortages in supply. In such instances, we reserve the right to include substitutes of similar quality, style and value to ensure that you receive a beautiful Kapruka flower arrangement",
+        price:3920,
+        imageUrl:"https://www.kapruka.com/cdn-cgi/image/width=340,quality=92,f=auto/shops/flowershop/flowerImages/zooms/1517487129417__DSC2211_M.JPG",
+    },
+    {
+        name:'FERRERO ROCHER - 24 PIECES BOX - 300G',
+        description:"24 pieces box Wrapped with an elegant ribbon 300g",
+        price:2750,
+        imageUrl:"https://www.kapruka.com/cdn-cgi/image/width=340,quality=92,f=auto/shops/flowershop/flowerImages/zooms/1517487129417__DSC2211_M.JPG",
+    },
+    {
+        name:'18KT YELLOW GOLD PENDANT - DIAMOND DREAMS - PENDENT',
+        description:"Diamond Dreams produces intricate pieces of fine Diamond Jewelry through combining latest technology with years of expertise. Our collection of exclusive and fine jewelry caters to a wide segment of the Sri Lankan women 18kt Yellow Gold / Gold WT 0.69G / Dias 25 ( 0.129CTS) Product Code: NV/1328/P-Y3",
+        price:50150,
+        imageUrl:"https://www.kapruka.com/cdn-cgi/image/width=700,quality=92,f=auto/shops/specialGifts/additionalImages/large/1545997533731_DSC_9703_M.jpg",
+    },
+    {
+        name:'TOP HANDLE TOTES BAG, SHOULDER HANDBAG FOR WOMEN (GREEN)',
+        description:"Put a personal touch to your daily handbag with our classy handbag collection. Our handbags feature detailed craftsmanship, subtle zip closures and a structured shape to keep all your daily necessities, and possibly paperwork intact. We offer handbags in every style, shape and color. It`s a perfect accessory for your outfits. Match it with pair of heels or sandals to give a complete look. Dimention (LxWxH) - 10.5x4x9.5 (inches)",
+        price:5000,
+        imageUrl:"https://images-na.ssl-images-amazon.com/images/I/71BEb7buriL._SS400_.jpg",
+    },
+];
+
+
+module.exports=products;
+   
+    
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
-
 const express =require('express');
+const connectDB=require('./config/db');
+
+
+connectDB();
+
 const app= express();
 
 const PORT=process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log('server is running on port',PORT))
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`));

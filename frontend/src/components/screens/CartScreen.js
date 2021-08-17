bb0b1bc43bd6e547d8cabc220c5ef3d66e5e2eff
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-17 12:58:54 +0530

    Product screen completed

--- frontend/src/components/screens/CartScreen.js
+++ frontend/src/components/screens/CartScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import CartItem from '../../components/CartItem';
 export default function CartScreen() {
     return (
         <div className="cartscreen">
-           cartScreen 
+           <div className='cartscreen_left'>
+               <h2>Shopping Cart</h2>
+                <CartItem/>
+           </div>
+           <div className='cartscreen_right'>
+
+           </div>
         </div>
     )
 }
